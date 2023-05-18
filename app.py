6aafeb9c3e0d5eabcce9a03b42bc001ec76c2d08
widderslainte <widderslainte@gmail.com>
2023-05-18 09:52:34 -0400

    fixing dockerfile, starting hireling

--- app.py
+++ app.py
@@ -164,7 +164,16 @@ def route_spoor():
     output = check_spoor()
     return output
 
-# @app.route('/hirelings')
-# def route_spoor():
-#     output = check_character_class()
-#     return output
+@app.route('/hirelings')
+def route_hirelings():
+    # output = check_character_class()
+    # return output
+    section = "Hireling"
+    # item1 = check_character_class()
+    item1 = check_hireling_type()
+    item2 = check_common_ancestry()
+    item3 = check_gender()
+    item4 = check_alignment()
+    # item5 = check_hp()
+    item5 = ""
+    return render_template('generic5.html', title=section, section=section, item1=item1, item2=item2, item3=item3, item4=item4, item5=item5)
--- hirelings.py
+++ hirelings.py
@@ -1,4 +1,5 @@
 from dice import *
+hd = 6
 
 # Village or Small Town
 ## Number available
@@ -9,63 +10,92 @@ from dice import *
 ## Determine hireling or henchmen
 
 # Determine hireling type
-# * Torchbearer
-# * Man-at-arms
-# * War dog
+def check_hireling_type():
+    # roll = roll1d6()
+    # if roll <= 2:
+    #     hireling_type="Man-at-arms"
+    # elif roll == 6:
+    #     hireling_type="War dog"
+    # else:
+    #     hireling_type="Torchbearer"
+    # return hireling_type
+    hireling = {
+        'War dog': 8,
+        'Man-at-arms': 6,
+        'Man-at-arms': 6,
+        'Torchbearer': 4,
+        'Torchbearer': 4,
+        'Torchbearer': 4,
+    }
+    hireling_type = random.choice(list(hireling.keys()))
+    hit_points = random.randint(1, hireling[hireling_type]) + (hireling[hireling_type] // 2)
+    return "Type: " + hireling_type, 'HP: ' + str(hit_points)
+
 
 # Determine henchmen type
 def check_character_class():
     roll = roll1d10()
     if roll == 1:
         character_class="Magician"
+        hd = 4
     elif roll == 2:
         character_class="Priest"
+        hd = 8
     elif roll == 3:
         character_class="Priest"
+        hd = 8
     elif 4 < roll <= 6:
         character_class="Burglar"
+        hd = 6
     else:
-        character_class="Warrior"  
+        character_class="Warrior"
+        hd = 10
+    return 'Class: ' + character_class  
 
 # Ancestry
-# def check_common_ancestry():
-#     roll = roll1d10()
-#     if roll == 1:
-#         then ancestry="Ancestry: Maetah"
-#     elif roll == 2:
-#         then ancestry="Ancestry: Chlendi"
-#     elif roll == 3:
-#         then ancestry="Ancestry: Heteri"
-#     elif roll == 4:
-#         then ancestry="Ancestry: Mindat"
-#     elif roll == 5:
-#         then ancestry="Ancestry: Eskla"
-#     else:
-#         then ancestry="Ancestry: Dominant for Region"   
+def check_common_ancestry():
+    roll = roll1d10()
+    if roll == 1:
+        ancestry="Ancestry: Maetah"
+    elif roll == 2:
+        ancestry="Ancestry: Chlendi"
+    elif roll == 3:
+        ancestry="Ancestry: Heteri"
+    elif roll == 4:
+        ancestry="Ancestry: Mindat"
+    elif roll == 5:
+        ancestry="Ancestry: Eskla"
+    else:
+        ancestry="Ancestry: Dominant for Region"
+    return ancestry   
 
 # HP
-# def check_hp():	
+def check_hp():
+    hp = str(random.randint(1,hd))
+    return 'HP: ' + hp
 
 # Sex	
-# def check_alignment():
-#     roll = roll1d6()
-#     if roll < 4:
-#         then alignment="Sex: Female"
-#     else:
-#         then alignment="Sex: Make"
+def check_gender():
+    roll = roll1d6()
+    if roll < 4:
+        alignment="Gender: Female"
+    else:
+        alignment="Gender: Male"
+    return alignment
 
 # Weapon	
 # Armor	
-# Alignment	= ["Chaos", "Neutral", "Law"]
 
-# def check_alignment():
-#     roll = roll1d6()
-#     if roll == 1:
-#         then alignment="Alignment: Chaos"
-#     elif roll == 6:
-#         then alignment="Alignment: Law"
-#     else:
-#         then alignment="Alignment: Neutral"
+def check_alignment():
+    Alignment	= ["Chaos", "Neutral", "Law"]
+    roll = roll1d6()
+    if roll == 1:
+        alignment="Alignment: Chaos"
+    elif roll == 6:
+        alignment="Alignment: Law"
+    else:
+        alignment="Alignment: Neutral"
+    return alignment
 
 # Background	
 # Possessions & Knowledge	
